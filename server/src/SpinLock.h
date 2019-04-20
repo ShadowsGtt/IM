5ea663011cc386b0ae7132e4301392ff18a922a4
Tian Gao <1025814447@qq.com>
2019-04-20 22:57:11 +0800

    fix SpinLock

--- server/src/SpinLock.h
+++ server/src/SpinLock.h
@@ -7,17 +7,18 @@ class SpinLock
 {
 
 public:
-    Spinlock() : flag(ATOMIC_FLAG_INIT) {}
+    SpinLock() : flag_(ATOMIC_FLAG_INIT) {}
 
-    void lock(){
-        while ( flag.test_and_set() );
+    bool Lock(){
+        while ( flag_.test_and_set(std::memory_order_acquire) );
+        return true;  
     }
 
-    void unlock(){
-        flag.clear();
+    void unLock(){
+        flag_.clear(std::memory_order_release);
     }
 private:
-    std::atomic_flag flag;
+    std::atomic_flag flag_;
 
 };
 
